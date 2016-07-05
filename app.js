00bce612d0e760bf5eda2fb5566d6ac36bbc05db
RadeshShiwlochan <radesh0430@gmail.com>
2016-07-04 23:55:59 -0400

    changes a route from get to post to create a user

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ var express = require('express'),
     MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-var dblocation = 'mongodb://localhost:27017/customer';
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');    
 
-app.get('/', function(req, res) {
+MongoClient.connect(dblocation, function(err, db) {
+
+	assert.equal(null, err);
+	console.log("Successfully connected to the server");
+	app.POST('/home', function(res, req) {
+
+	})
+
+	// db.collection('info').find({"name":"mickey mouse"}).toArray(function(err, docs) {
+	// 	docs.forEach(function(doc) {
+	// 		if(doc.name === "mickey mouse") {
+	// 		console.log("Found a " + doc.name + " in the database");
+	// 	    } else {
+	// 	    	console.log("Person is not in the database");
+	// 	    }
+	// 	});
+	// 	db.close();
+	// });
+	// console.log("Called find()");
+}); 
+
+app.get('/home', function(req, res) {
+	var nameFromForm = req.query.name;
 	res.render('home');
 });
 
@@ -25,19 +45,31 @@ app.get('/inspiration', function(req, res) {
 	res.render('Inspiration quotes inspire');
 });
 
-MongoClient.connect(dblocation, function(err, db) {
+var getNameValue = app.get('/home', function(req,res) {
+	var getName  = req.query.name;
 
-	assert.equal(null, err);
-	console.log("Successfully connected to the server");
+	return getName;
+});
 
-	db.collection('info').find({}).toArray(function(err, docs) {
-		docs.forEach(function(doc) {
-			console.log(doc.name);
-		});
-		db.close();
-	});
-	console.log("Called find()");
-});    
+var dblocation = 'mongodb://localhost:27017/customer';
+
+// MongoClient.connect(dblocation, function(err, db) {
+
+// 	assert.equal(null, err);
+// 	console.log("Successfully connected to the server");
+
+// 	db.collection('info').find({"name":"mickey mouse"}).toArray(function(err, docs) {
+// 		docs.forEach(function(doc) {
+// 			if(doc.name === "mickey mouse") {
+// 			console.log("Found a " + doc.name + " in the database");
+// 		    } else {
+// 		    	console.log("Person is not in the database");
+// 		    }
+// 		});
+// 		db.close();
+// 	});
+// 	console.log("Called find()");
+// });    
 
 app.listen(27017, function() {
 	console.log("app listening on port 27017");
